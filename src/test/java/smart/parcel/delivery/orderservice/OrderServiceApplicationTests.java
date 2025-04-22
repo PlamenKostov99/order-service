27814e724e1289c7d6d8f0e3e9909d054eede7da
PlamenKostov99 <A2vMQL@T4@rwzju>
2025-04-22 13:34:12 +0300

    initial commit

---
+++ src/test/java/smart/parcel/delivery/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package smart.parcel.delivery.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
